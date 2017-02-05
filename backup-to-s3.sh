9ca0ec4d3231d50ba8cf61ea82e40308a8e9c257
Drazen Urch <github@drazenur.ch>
2017-02-05 20:12:37 +0100

    Fix backup script to include all

--- backup-to-s3.sh
+++ backup-to-s3.sh
@@ -6,5 +6,5 @@ DOW=$(date +%A | tr '[:upper:]' '[:lower:]')
 FN=jira_${DOW}
 FNEXT=${FN}.tar.gz
 
-tar -czvf ${JIRA_HOME}/${FNEXT} ${JIRA_HOME}/data
-aws s3 cp ${JIRA_HOME}/${FNEXT} s3://${S3_BUCKET}/ --region eu-central-1
+tar -czvf /tmp/${FNEXT} ${JIRA_HOME}
+aws s3 cp /tmp/${FNEXT} s3://${S3_BUCKET}/ --region eu-central-1
