20b97e81ab201eff74a5278b2f920595a9c93168
Drazen Urch <github@drazenur.ch>
2017-02-19 00:40:52 +0100

    Fix entrypoint path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 cd /tmp
 aws s3 cp s3://${S3_BUCKET}/jira_latest.tar.gz . --region eu-central-1
 tar -zxvf jira_latest.tar.gz
-mv var/atlassian /var/atlassian 
+mv var/atlassian/jira /var/atlassian 
 /opt/atlassian/jira/bin/catalina.sh run
