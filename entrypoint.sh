e5ccd795f09bc6efb2794eb35042aa3bbaf42cfb
Drazen Urch <github@drazenur.ch>
2017-02-19 00:18:00 +0100

    Stop making stupid bugs, its late

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp
-aws s3 cp s3://${S3_BUCKET}/jira_thursday.tar.gz . --region eu-central-1
+aws s3 cp s3://${S3_BUCKET}/jira_latest.tar.gz . --region eu-central-1
 tar -zxvf jira_latest.tar.gz
 mv var/atlassian /var/atlassian 
