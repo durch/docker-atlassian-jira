7a802870a582bbb4f643cbb021c189560f463cfd
tdebus <tdebus@gmail.com>
2017-07-27 15:10:08 +0200

    Restore updated for APP_TAG

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 cd /tmp
 # Get latest backup from S3 and replace whatever is in /var/atlassian/jira
-aws s3 cp s3://${S3_BUCKET}/jira_latest.tar.gz . --region eu-central-1
-tar -zxvf jira_latest.tar.gz
+aws s3 cp s3://${S3_BUCKET}/${APP_TAG}_latest.tar.gz . --region eu-central-1
+tar -zxvf ${APP_TAG}_latest.tar.gz
 cp -r var/atlassian/jira /var/atlassian/ 
-# Start cron
-/etc/init.d/cron start
+# Start cron - unused as scheduled from nodes
+# /etc/init.d/cron start
 # Run catalina in foreground
 /opt/atlassian/jira/bin/catalina.sh run
